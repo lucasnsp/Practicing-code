9897ce8c27f8c687aa135b901342efbd52f5f64b
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-01-30 11:56:18 -0300

    Add files via upload

---
+++ 5 - If Else/index.js
@@ -0,0 +1,16 @@
+/*const idade = prompt("Informe a sua idade");
+
+if (idade >= 18) {
+  alert("Você é maior de idade");
+} else if (idade > 12) {
+  alert("Você é menor de idade");
+} else if (idade < 12) {
+  alert("Você é criança");
+}
+*/
+
+// Operador Ternário
+
+const resultado = 6 === 6 ? "Verdadeiro" : "Falso";
+
+console.log(resultado);
