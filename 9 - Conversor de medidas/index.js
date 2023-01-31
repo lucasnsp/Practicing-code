280ec417badd5beada680496b890c2a88b7ac738
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-01-31 19:57:36 -0300

    Add files via upload

---
+++ 9 - Conversor de medidas/index.js
@@ -0,0 +1,38 @@
+const value = prompt("Por favor Insira um valor em metros para conversão. ");
+
+const medida = prompt(
+  "Para qual tipo de medida você deseja converter?  \n\nA) Milímetros (mm) \nB) Centímetros (cm) \nC) Decímetros (dm) \nD) Decâmetros (dam) \nE) Hectômetros (hm) \nF) Quilômetros (km)"
+);
+
+switch (medida) {
+  case "A":
+    alert(
+      "Conversão em progresso: \n\nO resultado é: \n" + value * 1000 + " mm "
+    );
+    break;
+  case "B":
+    alert(
+      "Conversão em progresso: \n\nO resultado é: \n" + value * 100 + " cm "
+    );
+    break;
+  case "C":
+    alert("Conversão em pogresso: \n\nO resultado é: " + value * 10 + " dm ");
+    break;
+  case "D":
+    alert("Conversão em pogresso: \n\nO resultado é: " + value * 0.1 + " dam ");
+    break;
+  case "E":
+    alert(
+      "Conversão em progresso: \n\nO resultado é: " + value * 0.01 + " hm "
+    );
+    break;
+  case "F":
+    alert(
+      "Conversão em progresso: \n\nO resultado é: " + value * 0.001 + " km "
+    );
+    break;
+  default:
+    alert(
+      "OPÇÃO INVÁLIDA! \n\nPor favor inserir uma resposta válida, se atentar ao capslock."
+    );
+}
