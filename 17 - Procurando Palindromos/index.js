5d709d022a298e399cbcd04b57d1ab64c36a4413
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-02-02 12:11:12 -0300

    Add files via upload

---
+++ 17 - Procurando Palindromos/index.js
@@ -0,0 +1,27 @@
+/*
+## Procurando Palíndromos
+
+Escreva um programa em javascript que seja capaz de identificar se uma palavra é um palíndromo. 
+Um palíndromo é uma palavra que lida de trás para frente possui as mesmas letras na mesma ordem. 
+O programa deve iniciar pedindo que seja informada uma palavra e então deve exibir uma mensagem 
+dizendo se aquela palavra é ou não um palíndromo. 
+Caso não seja um palíndromo, o programa deve mostrar a palavra lida da esquerda para direita e da direita 
+para esquerda.
+*/
+
+const palavra = prompt("Informe uma palavra:");
+let palavraInvertida = "";
+
+// Utilizando palavra.length - 1 para pegar a ultima letra da palavra
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+  palavraInvertida += palavra[i];
+}
+
+if (palavra === palavraInvertida) {
+  alert(palavra + " é um palíndromo!");
+} else {
+  alert(
+    palavra + " não é um palíndromo!\n\n" + palavra + " !== " + palavraInvertida
+  );
+}
